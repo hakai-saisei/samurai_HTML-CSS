dd044f8b0525b4603c168e7398a1bae425359bc2
hakai-saisei <hakai@mizoguchisonkorenoMacBook-Pro.local>
2021-07-26 21:47:51 +0900

    Vuetify ７月２６日

---
+++ pages/index.vue
@@ -0,0 +1,133 @@
+<template>
+
+<!-- App.vue -->
+
+<v-app>
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <!-- -->
+  </v-app-bar>
+
+  <!-- アプリケーションのコンポーネントに基づいてコンテンツのサイズを決定 -->
+  <v-main>
+
+    <!-- アプリケーションに適切なgutterを提供します -->
+    <v-container fluid>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    ></v-carousel-item>
+  </v-carousel>
+
+  <div class="text-center">
+    <v-btn
+      class="ma-2"
+      outlined
+      color="indigo"
+    >
+      Outlined Button
+    </v-btn>
+    <v-btn
+      class="ma-2"
+      outlined
+      fab
+      color="teal"
+    >
+      <v-icon>mdi-format-list-bulleted-square</v-icon>
+    </v-btn>
+    <v-btn
+      class="ma-2"
+      outlined
+      large
+      fab
+      color="red"
+    >
+      <v-icon>mdi-mouse</v-icon>
+    </v-btn>
+  </div>
+
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="290"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Open Dialog
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title class="text-h5">
+          Use Google's location service?
+        </v-card-title>
+        <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Disagree
+          </v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Agree
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+
+      <!-- vue-routerを使用している場合 -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+      }
+    },
+  }
+
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,26 @@
+<!-- App.vue -->
+
+<v-app>
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <!-- -->
+  </v-app-bar>
+
+  <!-- アプリケーションのコンポーネントに基づいてコンテンツのサイズを決定 -->
+  <v-main>
+
+    <!-- アプリケーションに適切なgutterを提供します -->
+    <v-container fluid>
+
+      <!-- vue-routerを使用している場合 -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app
