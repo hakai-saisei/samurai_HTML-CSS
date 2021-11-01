ae1c2933d02c335f60c0281dce5006c95a13b6dc
hakai-saisei <hakai@mizoguchisonkorenoMacBook-Pro.local>
2021-11-01 22:42:21 +0900

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  router: {
+    base: 'samurai_HTML-CSS'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
